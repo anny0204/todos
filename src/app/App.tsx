8fe5038ba712a7aa3198305a3fdd0485cb73b1b6
Anna Kovalenko <anna.kovalenko@hmbd.pl>
2019-05-01 14:50:18 +0200

    overrided defaults

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.sass';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        Hello from App
+      </div>
+    );
+  }
+}
+
+export default App;
